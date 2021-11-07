9b70a9a7dd9e49489afcd845d17dcdf6c2ee06b8
EChilin5 <edgar3ac@gmail.com>
2021-11-07 13:02:23 -0800

    created profile activity

---
+++ app/src/main/java/com/example/itv/setUpProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.itv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class setUpProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_up_profile)
+    }
+}
