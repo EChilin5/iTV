6e76a7dedb125af0ef8d596f7b4b4ae2d540b6ea
EChilin5 <edgar3ac@gmail.com>
2021-12-12 17:36:41 -0800

    remove unused code that caused compilation error

--- app/src/main/java/com/example/itv/fragment/CameraFragment.kt
+++ app/src/main/java/com/example/itv/fragment/CameraFragment.kt
@@ -16,8 +16,7 @@ import com.example.itv.databinding.FragmentCameraBinding
 import com.example.itv.image.FirebaseStorageManager
 import com.example.itv.overlayfood
 import com.google.firebase.functions.FirebaseFunctions
-import com.google.firebase.ml.vision.FirebaseVision
-import com.google.firebase.ml.vision.common.FirebaseVisionImage
+//import com.google.firebase.ml.vision.FirebaseVision
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
@@ -72,15 +71,7 @@ class CameraFragment : Fragment() {
         intUi()
 
         setUpBarChart()
-//        binding.btnCapture.setOnClickListener{
-//            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//            startActivityForResult(intent, 123)
-//
-//        }
-//        binding.btnSave.setOnClickListener {
-//            Toast.makeText(context, "selected button", Toast.LENGTH_LONG).show()
-//            openFoodItem()
-//        }
+
 
     }
 
@@ -106,19 +97,7 @@ class CameraFragment : Fragment() {
     }
 
     private fun intUi() {
-//        binding.btnCapture.setOnClickListener{
-//            captureImageCam()
-//        }
-//        binding.btnSave.setOnClickListener {
-//            val imgURI = binding.btnSave.tag
-//            if(imgURI == null){
-//                Toast.makeText(context, "Please select Image", Toast.LENGTH_SHORT).show()
-//            }else{
-//                context?.let { it1 -> FirebaseStorageManager().uploadImage(it1, imgURI as Uri) }
-//
-//            }
-//
-//        }
+
     }
 
     private fun captureImageCam() {
@@ -131,7 +110,7 @@ class CameraFragment : Fragment() {
 
     private fun openFoodItem() {
 
-        var dialog = overlayfood()
+        val dialog = overlayfood()
         dialog.show(childFragmentManager, "overlay")
     }
 
@@ -141,10 +120,6 @@ class CameraFragment : Fragment() {
             //Image Uri will not be null for RESULT_OK
             val uri: Uri = data?.data!!
 
-
-            // Use Uri object instead of File to avoid storage permissions
-//            binding.ivImageText.setImageURI(uri)
-//            binding.btnSave.setTag(uri)
             context?.let { FirebaseStorageManager().uploadImage(it, uri) }
 
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
@@ -154,90 +129,6 @@ class CameraFragment : Fragment() {
         }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (requestCode == 123){
-//            if(resultCode== Activity.RESULT_OK){
-////                val photo = data?.extras?.get("data") as Bitmap
-////                binding.ivImageText.setImageBitmap(photo)
-////                val image = FirebaseVisionImage.fromBitmap(photo)
-////                getImageText(image)
-//
-////                photo = scaleBitmapDown(photo, 640)
-////                //convert bitmap to base64 encoded string
-////                val byteArrayOutputStream = ByteArrayOutputStream()
-////                photo.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
-////                val imageBytes: ByteArray = byteArrayOutputStream.toByteArray()
-////                val base64encoded = Base64.encodeToString(imageBytes, Base64.NO_WRAP)
-//
-//                val bundle: Bundle = data?.extras!!
-//                //form bundle, extract the image
-//                //form bundle, extract the image
-//                bundle["data"]
-//                val bitmap = bundle["data"] as Bitmap?
-//                //SET IMAGE IN IMAGE VIEW
-//                //SET IMAGE IN IMAGE VIEW
-//                binding.ivImageText.setImageBitmap(bitmap)
-//                //proccess the Image
-//
-//                //1. create a firbaseVisionImage object from a bitmap object
-//                //proccess the Image
-//
-//                //1. create a firbaseVisionImage object from a bitmap object
-//                val image = FirebaseVisionImage.fromBitmap(bitmap!!)
-//                // get an instance of firebasevISION
-//                // get an instance of firebasevISION
-//                val firebaseVision = FirebaseVision.getInstance()
-//                //3.Create instance of the firebaseVisionfire
-//                //3.Create instance of the firebaseVisionfire
-//                val firebaseVisionTextRecognizer = firebaseVision.onDeviceTextRecognizer
-//                //4 create task to proccess image
-//                //4 create task to proccess image
-//                val task = firebaseVisionTextRecognizer.processImage(image)
-//
-//                task.addOnSuccessListener { firebaseVisionText ->
-//                    val s = firebaseVisionText.text
-//                    // tvText.setText(s)
-//                    Toast.makeText(context, s, Toast.LENGTH_SHORT).show()
-//                    Log.i("CameraFragment", s)
-//
-//
-//
-//                }
-//                task.addOnFailureListener { e ->
-//                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
-//                    Log.i("CameraFragment" , e.message.toString())
-//
-//                }
-//
-//
-//            }
-//        }
-//    }
-
-    private fun getImageText(image: FirebaseVisionImage) {
-        val textRecognizer = FirebaseVision.getInstance().cloudTextRecognizer
-        textRecognizer.processImage(image)
-            .addOnSuccessListener {
-                val it = it.text
-//                binding.tvImageText.text = it
-               Log.i("CameraFragment", it)
-                // calories
-                // protein
-                // fat
-                // carbs
-                // create a hashmap
-
-
-                // ...
-            }
-            .addOnFailureListener {
-                // Task failed with an exception
-                // ...
-                Toast.makeText(context, "failed" + it.message, Toast.LENGTH_LONG).show()
-                Log.i("CameraFragment" , it.message.toString())
-            }
-    }
 
     /// scale down image in order to save bandwith
     // this is an optional
